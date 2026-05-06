929436fa12a05b4758551e84b7c121a9c1bb08b8
Eyal Sooliman <soul3510@gmail.com>
2026-05-06 14:11:26 +0300

    ghngfn

--- tests/api/posts.spec.ts
+++ tests/api/posts.spec.ts
@@ -1,21 +1,54 @@
-import {test,expect} from '@playwright/test';
-
-test.describe('API tests', () => {
-    test.beforeEach(async ({ page }) => {
-    await page.goto('https://demo.playwright.dev/todomvc');
-  });
+import { test, expect } from '@playwright/test';
 
 
-    test('should fetch all posts', async ({ request }) => {
-        const response = await request.get('https://jsonplaceholder.typicode.com/posts');
-        expect(response.status()).toBe(200);
-        const body=await response.json();
-        expect(body.length).toBeGreaterThan(0);
-        console.log('Fetched all posts successfully');
+test('should fetch all posts', async ({ request }) => {
 
-        expect(body[0]).toHaveProperty('id');
-        expect(body[0]).toHaveProperty('title');
-        expect(body[0]).toHaveProperty('body');
- });
 
-  });
+  const response = await request.get('https://jsonplaceholder.typicode.com/posts');
+  expect(response.status()).toBe(200);
+  const body = await response.json();
+  expect(Array.isArray(body)).toBeTruthy();
+  expect(body.length).toBeGreaterThan(0);
+  console.log('Fetched all posts successfully');
+
+
+  for (const post of body) {
+    expect(post).toHaveProperty('userId');
+    expect(post).toHaveProperty('id');
+    expect(post).toHaveProperty('title');
+    expect(post).toHaveProperty('body');
+    console.log('Verified properties of the first post successfully');
+
+    expect(post.userId).toEqual(expect.any(Number));
+    expect(post.id).toEqual(expect.any(Number));
+    const titleLength = post.title.length;
+    expect(titleLength).toBeGreaterThan(0);
+    const postLength = post.body.length;
+    expect(postLength).toBeGreaterThan(10);
+    console.log('Verified data types and content of the first post successfully');
+  }
+
+});
+
+
+test('Edge case - 404', async ({ request }) => {
+  const response = await request.get('https://jsonplaceholder.typicode.com/posts/999999');
+  expect(response.status()).toBe(404);
+  console.log('Edge case - 404: Post not found as expected');
+});
+
+
+test('Performance Response Time Test', async ({ request }) => {
+  const start = performance.now();
+  const response = await request.get('https://jsonplaceholder.typicode.com/posts');
+  const duration = performance.now() - start;
+  expect(response.status()).toBe(200);
+  expect(duration, `Response took ${duration}ms`).toBeLessThan(1000); // Expect response time to be less than 1 seconds
+  console.log(`Performance Test: Response time is ${duration} ms, which is within the acceptable range`);
+
+  const body = await response.json();
+  expect(body.length).toBeGreaterThan(50);
+  console.log(`Performance Test: Fetched ${body.length} posts successfully, which is more than 50 posts as expected`);
+
+});
+
--- tests/ui/todo.spec.ts
+++ tests/ui/todo.spec.ts
@@ -1,31 +1,66 @@
-import {test,expect} from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 
 
-  test.describe('Todo UI tests', () => {
-    test.beforeEach(async ({ page }) => {
+test.describe('Todo UI tests', () => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://demo.playwright.dev/todomvc');
   });
 
 
 
-    test('should add a new todo item', async ({ page }) => {
-        const input=page.getByPlaceholder('What needs to be done?');
-        await input.fill('Buy groceries');
-        await input.press('Enter');
-        const todoItem=page.getByText('Buy groceries');
-        await expect(todoItem).toBeVisible();
-        await expect(todoItem).toHaveCount(1);
-        await expect(todoItem.first()).toHaveText('Buy groceries');
-        console.log('Todo item added successfully');
-    });
+  test('should add a new todo item', async ({ page }) => {
 
-    test('should mark a todo item as completed', async ({ page }) => {  
-        const todoInput = page.getByPlaceholder('What needs to be done?');
-        const todoItem = page.locator('.todo-list li');
-        await todoInput.fill('Buy groceries');
-        await todoInput.press('Enter');
-        await todoItem.getByRole('checkbox').check();
-        await expect(todoItem.first()).toHaveClass(/completed/);
-    });
+    const taskA = 'Task A';
+    const taskB = 'Task B';
+    const taskC = 'Task C';
+    let tasks = [taskA, taskB, taskC];
+
+    const input = page.getByPlaceholder('What needs to be done?');
+
+    for (const task of tasks) {
+      
+      await input.fill(task);
+      await input.press('Enter');
+      const todoItem = page.getByText(task);
+      await expect(todoItem).toBeVisible();
+      await expect(todoItem.first()).toHaveText(task);
+      console.log(`Todo item: tasks added successfully`);
+    }
+
+    let allTodoItems = page.locator('.todo-list li');
+    await expect(allTodoItems).toHaveCount(tasks.length);
+    await expect(allTodoItems).toHaveText(tasks);
+
+    console.log(`All todo items: ${tasks.length} tasks added successfully`);
+    const inputTaskB = page.getByRole('listitem').filter({ hasText: 'Task B' }).getByLabel('Toggle Todo');
+    await inputTaskB.check();
+
+
+    const taskBItem  = page.getByRole('listitem').filter({ hasText: 'Task B' });
+    await expect(taskBItem).toHaveClass(/completed/);
+    console.log(`Task B: marked as completed successfully`);
+
+    const taskAItem  = page.getByRole('listitem').filter({ hasText: 'Task A' });
+    const taskCItem  = page.getByRole('listitem').filter({ hasText: 'Task C' });
+    await expect(taskAItem).not.toHaveClass(/completed/);
+    await expect(taskCItem).not.toHaveClass(/completed/);
+    console.log(`Task A and Task C are NOT completed`); 
+
+
+    await page.getByRole('link', { name: 'Completed' }).click();
+    let allCompletedTodoItems = page.locator('.todo-list li');
+    await expect(allCompletedTodoItems).toHaveCount(1);
+    await expect(allCompletedTodoItems).toHaveText('Task B');
+    console.log(`Task A and Task C are NOT completed`);
+
+    const itemLeft = page.locator('.todo-count').filter({ hasText: '2 items left' });
+    await expect(itemLeft).toContainText('2 items left');
+    console.log(`Verifyied '2 items left'`);
+
+  });
+
+  test('should mark Task B todo item as completed', async ({ page }) => {
+    // const input = page.getByRole('listitem').filter({ hasText: 'Task B' }).getByLabel('Toggle Todo').check();
+  });
 });
